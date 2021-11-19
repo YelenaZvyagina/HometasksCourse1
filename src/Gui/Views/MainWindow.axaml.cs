74cbed337564279ad8b39e21588c117f457931e0
yelena <yelenazvyagina@gmail.com>
2021-11-19 03:17:01 +0300

    fixed mentioned points

--- src/Gui/Views/MainWindow.axaml.cs
+++ src/Gui/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -13,6 +14,7 @@ namespace Gui.Views
         private readonly TextBox _console;
         public static TextBox StatusBar = null!;
         public static string? Filename;
+        private readonly MenuItem _run;
 
         public MainWindow()
         {
@@ -22,6 +24,7 @@ namespace Gui.Views
             _console = this.Find<TextBox>( "Console");
             StatusBar = this.FindControl<TextBox>("StatusBar");
             Filename = "";
+            _run = this.FindControl<MenuItem>("Run");
         }
 
         public static readonly MainWindow main = new ();
@@ -36,6 +39,10 @@ namespace Gui.Views
             Dispatcher.UIThread.Post(() => _console.Text += s + '\n');
         }
 
+        [SuppressMessage("ReSharper.DPA", "DPA0001: Memory allocation issues")]
+        [SuppressMessage("ReSharper.DPA", "DPA0001: Memory allocation issues")]
+        [SuppressMessage("ReSharper.DPA", "DPA0002: Excessive memory allocations in SOH", MessageId = "type: System.String")]
+        [SuppressMessage("ReSharper.DPA", "DPA0002: Excessive memory allocations in SOH", MessageId = "type: System.String")]
         private void Run(object? sender, RoutedEventArgs routedEventArgs)
         {
             if (string.IsNullOrEmpty(CodeInput.Text))
@@ -43,6 +50,8 @@ namespace Gui.Views
                 StatusBar.Text = "Empty entry, nothing to compute";
                 return;
             }
+
+            _run.IsEnabled = false;
             StatusBar.Text = "Computing..";
             var task = new Task(() =>
             {
@@ -53,6 +62,7 @@ namespace Gui.Views
                     {
                         StatusBar.Text = "Computed successfully";
                         _console.Text += "----------------------------------- \n";
+                        _run.IsEnabled = true;
                     });
                 }
                 catch (Exception ex)
@@ -60,6 +70,7 @@ namespace Gui.Views
                     Dispatcher.UIThread.Post(() =>
                     {
                         StatusBar.Text = ex.Message;
+                        _run.IsEnabled = true;
                     });
                 }
             });
